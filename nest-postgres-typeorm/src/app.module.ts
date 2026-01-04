fb3a57dcbc92cd11f169371faaf27e635eb334eb
Umair Tariq <86774981+tariqumair017@users.noreply.github.com>
2026-01-04 18:32:24 +0500

    db connection update

--- nest-postgres-typeorm/src/app.module.ts
+++ nest-postgres-typeorm/src/app.module.ts
@@ -3,19 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmployeeModule } from './employee/employee.module';
-
+console.log("process.env.DATABASE_URL", process.env.DATABASE_URL);
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
     }), 
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }), 
     UserModule, EmployeeModule
   ],
